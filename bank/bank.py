01151e31cb80a1449a25ec30910b4b935fa58b29
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 22:13:42 +0000

    Mon, Aug 14, 2023, 5:13 PM -05:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,1 +1,8 @@
-input("Greeting: ").strip().lower().replace(" ","")
+greeting = input("Greeting: ").strip().lower().replace(" ","")
+
+if greeting == "Hello":
+    print($0)
+      elif greeting == "H"
+      print($20)
+            else:
+            print($100)
