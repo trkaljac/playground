5bed9c37d03bf4efa9507ed9e8d847d3f699b5f3
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 17:17:25 +0000

    Mon, Aug 14, 2023, 12:17 PM -05:00

--- playback.py
+++ playback.py
@@ -1,2 +1,7 @@
-text =  input()
-print(input,sep="...")
+def slow_down_text(text):
+    return text.replace(' ', '...')
+
+if __name__ == "__main__":
+    user_input = input("Enter text: ")
+    slowed_down_text = slow_down_text(user_input)
+    print(slowed_down_text)
