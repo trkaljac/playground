11e0419c5d20102099dde98bc86064e445efe018
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 22:24:50 +0000

    Mon, Aug 14, 2023, 5:24 PM -05:00

--- bank/bank.py
+++ bank/bank.py
@@ -3,7 +3,7 @@ greeting = input("Greeting: ").strip().lower()
 
 if greeting == "hello":
     print("$0")
-else greeting.startswith("h")
+elif greeting.startswith("h"):
     print("$20")
 else:
     print("$100")
