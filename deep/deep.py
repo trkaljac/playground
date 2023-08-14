1fda21891b87f7a2a3e4e6d89dbdb26776308a97
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 21:23:35 +0000

    Mon, Aug 14, 2023, 4:23 PM -05:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,4 +1,5 @@
-answer = input("What is the Answer to the Great Question of Life,the Universe,and Everything?").replace(" ","").lower()
+answer = input("What is the Answer to the Great Question of Life,the Universe,and Everything?")
+input().replace(" ","").lower()
 if answer in [ 42,"42","forty-two","forty two"]:
     print("Yes")
 else:
