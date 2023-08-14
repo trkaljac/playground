e415071534c763490d8c2c15ee7ace2cf637746d
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 22:47:56 +0000

    Mon, Aug 14, 2023, 5:47 PM -05:00

--- bank/bank.py
+++ bank/bank.py
@@ -3,7 +3,7 @@ greeting = input("Greeting: ")
 
 greating = greeting.strip().lower().replace(" ","").replace(",","")
 
-if len(greeting) >= 3 and greeting[0] == "hello":
+if greeting == "hello":
     print("$0")
 elif greeting.startswith("h"):
     print("$20")
