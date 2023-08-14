c0f31776b8541780166bd2857220aacb8bee62cd
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 20:54:10 +0000

    Mon, Aug 14, 2023, 3:54 PM -05:00

---
+++ deep/deep.py
@@ -0,0 +1,5 @@
+input("What is the Answer to the Great Question of Life,the Universe,and Everything?")
+    if input() = "42","forty-two","forty two"
+        print("Yes)
+        elif
+        print("No")
