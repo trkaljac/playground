c274cb58cb7057850a85cd79691f87180840be28
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 21:00:38 +0000

    Mon, Aug 14, 2023, 4:00 PM -05:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,5 +1,5 @@
-user_inp = input("What is the Answer to the Great Question of Life,the Universe,and Everything?")
-if user_inp.lower in [ "42","forty-two","forty two"]:
+answer = input("What is the Answer to the Great Question of Life,the Universe,and Everything?")
+if answer in [ "42","forty-two","forty two"]:
     print("Yes")
 else:
     print("No")
