76d401fc244103454008b342bc0624059ef562f3
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-14 19:52:21 +0000

    Mon, Aug 14, 2023, 2:52 PM -05:00

---
+++ new.py
@@ -0,0 +1,15 @@
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+
+def dollars_to_float(dollars):
+    return float(dollars)
+
+
+def percent_to_float(percent):
+    return float(percent)/100
+
+main()
