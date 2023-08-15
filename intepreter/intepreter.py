bb8343584670dbd2749cca6f543978c714b2f9b4
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-15 20:56:02 +0000

    Tue, Aug 15, 2023, 3:56 PM -05:00

--- intepreter/intepreter.py
+++ intepreter/intepreter.py
@@ -1,6 +1,16 @@
 intepreter = input("Expression: ")
-x, y, z = intepreter.split(" ")
+x,y,z = intepreter.split(" ")
+
 x = float(x)
 z = float(z)
-if y == ("+"):
-    print(x+z)
+
+if y == "+":
+    print(x + z)
+elif y=="-":
+    print(x - z)
+elif y=="/":
+    print(x/z)
+elif y == "*":
+    print(x*z)
+else:
+    print("Wrong input")
