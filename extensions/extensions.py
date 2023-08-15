3fe649a754642b7cee092c372e1d0f3080fa87df
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-15 03:35:24 +0000

    Mon, Aug 14, 2023, 10:35 PM -05:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,12 +1,19 @@
 
-.jpg
-.jpeg
-.png
-.pdf
-.txt
-.zip
+
 file = input("File name: ")
 file = file.strip()
 
-if file.endswith(.gif)
-    print("")
+if file.endswith(.gif):
+    print("image/gif")
+elif file.endswith(.jpg):
+    print("image/jpg")
+elif file.endswith(.jpeg):
+    print("image/jpeg")
+elif file.endswith(.png):
+    print("image/png")
+elif file.endswith(.pdf):
+    print("image/pdf")
+elif file.endswith(.txt):
+    print("image/txt")
+else file.endswith(.zip):
+    print("image/zip")
