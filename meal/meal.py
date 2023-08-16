36fcbf76ad5dcf678ce65ac9f826013448f3123a
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-16 21:35:02 +0000

    Wed, Aug 16, 2023, 4:35 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,16 @@
 def main():
-    ...
+    ...# get time from user
+    #convert function
+    #breakfast
+    #lunch
+    #dinner
 
 
 def convert(time):
     ...
-
+#get hours and minuts
+#convtr to a float
+#return the resolt to main function
 
 if __name__ == "__main__":
     main()
