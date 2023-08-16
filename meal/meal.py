6268dcea3b11501e7d36d5ec6000f84191ab9fd7
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-16 21:50:45 +0000

    Wed, Aug 16, 2023, 4:50 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,13 +2,16 @@ def main():
     ...# get time from user time = input("What time it is ? ")
     #convert function time = time.convert(time)
     #breakfast7:00 to 8:00   if
-    #lunch12:00 13:00 elif
-    #dinner18:00 19:00 else
+                            print("")
+    #lunch12:00 13:00       elif
+                            print("")
+    #dinner18:00 19:00      else
+                            print("")
 
 
 def convert(time):
     ...
-#get hours and minuts
+#get hours and minuts hours, minutes = time.split(":")
 #convtr to a float
 #return the resolt to main function
 
