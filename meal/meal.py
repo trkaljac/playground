b7596d76b3b1c8b65a9eec5edfd016afe2c6b9a2
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-16 22:15:38 +0000

    Wed, Aug 16, 2023, 5:15 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -3,9 +3,9 @@ question = input("What time it is ? ")
     #convert function time = time.convert(question)
     #breakfast7:00 to 8:00   if time >=7 and time<=8:
                             print("brekfest time")
-    #lunch12:00 13:00       elif
+    #lunch12:00 13:00       elif time >=12 and time <=13:
                             print("lunch time")
-    #dinner18:00 19:00      else
+    #dinner18:00 19:00      elif time>=18 and time <=19:
                             print("dinner time")
 
 
