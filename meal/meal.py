2bb95cf944a2dede22d60a59e837a8e308713309
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-16 22:22:04 +0000

    Wed, Aug 16, 2023, 5:22 PM -05:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,20 +1,20 @@
 def main():
     question = input("What time it is ? ")
     time = time.convert(question)
-if time >=7 and time<=8:
-    print("brekfest time")
-elif time >=12 and time <=13:
-    print("lunch time")
-elif time>=18 and time <=19:
-    print("dinner time")
+    if time >=7 and time<=8:
+        print("brekfest time")
+    elif time >=12 and time <=13:
+        print("lunch time")
+    elif time>=18 and time <=19:
+        print("dinner time")
 
 
 def convert(time):
     ...
-hours, minutes = time.split(":")
-hours = float(hours)
-con_minutes = float(minutes) / 60
-time = hours + con_minutes
+    hours, minutes = time.split(":")
+    hours = float(hours)
+    con_minutes = float(minutes) / 60
+    time = hours + con_minutes
     return time
 
 
