35e8d85781be18d1c878ef65012f39abaab6b38e
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-16 18:05:19 +0000

    Wed, Aug 16, 2023, 1:05 PM -05:00

--- stupid.py
+++ stupid.py
@@ -1,12 +1,11 @@
 i = 3
 while i != 0:
 
-    print ("/"
-           /
-          /
-         /------|---
-                |
-                |
-                                ")
+    print("     /")
+    print("    /")
+    print("   /")
+    print("  /")
+    print(" /")
+    print("/")
     i = i -1
 
