fc2698cd4b8d5f0e815adb88ef55aa8fe8db7903
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-17 02:53:07 +0000

    Wed, Aug 16, 2023, 9:53 PM -05:00

--- camel/camel.py
+++ camel/camel.py
@@ -2,7 +2,7 @@ question = input("camelCase: ")
 snake_case = ""
 for character in question:
     if character.isupper():
-        snake_case +="_"+character.lower()
+        snake_case += "_" + character.lower()
     else:
         snake_case += character
 
