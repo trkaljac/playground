17b8239d41af900d41c7fbdf938b352e190aa79b
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-19 02:07:09 +0000

    Fri, Aug 18, 2023, 9:07 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,12 +1,6 @@
-ins = input(f"Amaunt Due:{coke} " )
-coke == 50
-Inserty Coin
-Change Owed: 0
-accept 25,10,5
-if ins 25:
-    print(coke - 25)
-if ins 10:
-    print(coke -10)
-if ins 5:
-    print(coke - 5)
+coke = 50
+# Input the amount of money inserted
+ins = int(input(f"Amount Due: {coke} "))
+change_owed = ins - coke  # Calculate the change owed
 
+print(f"Change Owed: {change_owed}")
