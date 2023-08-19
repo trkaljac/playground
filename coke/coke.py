cba7f5c75c59401e3a2e6e4360f93a54c10f3c96
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-19 02:32:56 +0000

    Fri, Aug 18, 2023, 9:32 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -5,11 +5,13 @@ coke = 50
 amaunt_due = 0
 
 while amaunt_due < coke:
-    print(f"Amount Due: {coke - amaunt_due}")  # Corrected variable name here
-    ins = int(input("Insert Coin: "))
-    amaunt_due += ins
+    print(f"Amount Due: {coke - amaunt_due}")
+        if ins not in [5, 10, 25]:
+        break
+ins = int(input("Insert Coin: "))
 
-    if ins not in [5, 10, 25]:
-        ins = int(input("Insert Coin: "))
+amaunt_due += ins
+
+
 
 print("Change Owed: 0")
