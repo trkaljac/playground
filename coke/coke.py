54631703b4ad78a4b7915f819d87e756b0a0b552
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-19 02:35:32 +0000

    Fri, Aug 18, 2023, 9:35 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,17 +1,15 @@
 
-
-
 coke = 50
 amaunt_due = 0
 
 while amaunt_due < coke:
     print(f"Amount Due: {coke - amaunt_due}")
-        if ins not in [5, 10, 25]:
-        break
-ins = int(input("Insert Coin: "))
 
-amaunt_due += ins
+    ins = int(input("Insert Coin: "))
+    if ins not in [5, 10, 25]:
+        print()
+        continue  
 
-
+    amaunt_due += ins
 
 print("Change Owed: 0")
