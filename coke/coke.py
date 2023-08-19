efc581af9833fed0eccd2cdeed5da96fd9273637
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-19 02:11:49 +0000

    Fri, Aug 18, 2023, 9:11 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,6 +2,7 @@ coke = 50
 # Input the amount of money inserted
 
 ins = int(input(f"Amount Due: {coke} "))
+print("Insert Coin")
 amaunt_due = coke - ins  # Calculate the change owed
 
 print(f"Amaunt Due: {amaunt_due}")
