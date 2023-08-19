515a79f50eb874e78bc853b923283a5a3bd00fdc
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-19 02:05:17 +0000

    Fri, Aug 18, 2023, 9:05 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,3 +1,12 @@
-ins = input(f"Amaunt Due:, {coke} " end="")
+ins = input(f"Amaunt Due:{coke} " )
 coke == 50
-inserty Coin
+Inserty Coin
+Change Owed: 0
+accept 25,10,5
+if ins 25:
+    print(coke - 25)
+if ins 10:
+    print(coke -10)
+if ins 5:
+    print(coke - 5)
+
