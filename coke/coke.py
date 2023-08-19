09cc47b2139eba184ea9187411c917a5135e2190
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-19 02:22:13 +0000

    Fri, Aug 18, 2023, 9:22 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,13 +1,12 @@
+
+
+
 coke = 50
 amaunt_due = 0
+
 while amaunt_due < coke:
-    print(f"Amount Due: {coke - ins}")
+    print(f"Amount Due: {coke - amaunt_due}")  # Corrected variable name here
     ins = int(input("Insert Coin: "))
     amaunt_due += ins
 
 
-
-
-amaunt_due = coke - ins  # Calculate the change owed
-
-print(f"Amaunt Due: {amaunt_due}")
