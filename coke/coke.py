8cb9713f3a5129d8c0b47c488abe8576f45ac4cf
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-19 02:15:41 +0000

    Fri, Aug 18, 2023, 9:15 PM -05:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,8 +1,9 @@
 coke = 50
 # Input the amount of money inserted
-print(f"Amount Due:"{coke})
-ins = int(input("Insrt Coin: "))
-print("Insert Coin")
+print(f"Amount Due: {coke}")
+
+ins = int(input("Insert Coin: "))
+
 amaunt_due = coke - ins  # Calculate the change owed
 
 print(f"Amaunt Due: {amaunt_due}")
