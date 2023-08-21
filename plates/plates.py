c98aece724ad59c9be87d5f42b2b2512e45364ad
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-21 04:06:11 +0000

    Sun, Aug 20, 2023, 11:06 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -1,6 +1,12 @@
+#max 6 min 2
+#number on the end only
+#first number cant be 0
+#no ",","."," ",":"
+#all caps 
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
+
         print("Valid")
     else:
         print("Invalid")
