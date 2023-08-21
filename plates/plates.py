67d53ea872d2e46058d472532159b23cf1f93c97
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-21 04:10:35 +0000

    Sun, Aug 20, 2023, 11:10 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -2,7 +2,7 @@
 #number on the end only
 #first number cant be 0
 #no ",","."," ",":"
-#all caps 
+#all caps
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -14,6 +14,8 @@ def main():
 
 def is_valid(s):
     ...
+    if len(s) <2 or len(s) >6:
+        return False
 
 
 main()
