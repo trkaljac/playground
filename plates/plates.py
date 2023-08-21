e0b1e164f6c4a0f7f159c7c9c15f08413503ff2a
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-21 04:18:25 +0000

    Sun, Aug 20, 2023, 11:18 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -6,7 +6,7 @@
 invalid_characters = set(',. :')
 def is_valid(s):
     ...
-    if len(s) <2 or len(s) >6:
+    if len(s) < 2 or len(s) > 6:
         return False
     if not s [-1].isdigit():
         return False
