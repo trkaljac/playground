1a89467510a407a739aaa43c560e6e58a67619e3
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-21 04:59:57 +0000

    Sun, Aug 20, 2023, 11:59 PM -05:00

--- plates/plates.py
+++ plates/plates.py
@@ -8,16 +8,13 @@ def main():
 
 def is_valid(s):
     if 2 <= len(s) <= 6 and s.isalnum():
-        # Return true if characters are all letters
         if s.isalpha():
             return True
         else:
-            # Check for number in the middle
-            # (only if the first two characters are letters and the last character is number)
+
             if s[:2].isalpha() and s[-2:].isdigit():
                 for i in range(len(s)):
                     if s[i].isdigit():
-                        # Return false if number starts with 0 or the following character is letter
                         if s[i].startswith("0") or s[i:].isalpha():
                             return False
                         else:
