fe2144030cc7ef13694b69a22b37cb6c008987e7
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 06:23:17 +0000

    Sun, Aug 27, 2023, 1:23 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,13 +5,14 @@ fraction_input = input("Fraction: ").split('/')
 numerator = int(fraction_input[0])
 denominator = int(fraction_input[1])
 percentage = (numerator / denominator) * 100
-percentage = f"{percentage:.0f}%"
+
 
 if percentage <= 1:
     print("E")
 elif percentage >= 99:
     print("F")
 else:
+    percentage = f"{percentage:.0f}%"
     print(percentage)
 try:
     ...
