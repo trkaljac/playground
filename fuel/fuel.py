cd6f3be5e1c069117aa7fa6a883ae9c24e7f5bf9
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 05:25:47 +0000

    Sun, Aug 27, 2023, 12:25 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,7 +5,7 @@ import math
 fraction_input = input("Fraction: ").split('/')
 numerator = int(fraction_input[0])
 denominator = int(fraction_input[1])
-print((numerator/denominator)* 100)
+print((numerator/denominator)* 100 is {rounded_up})
 
 try:
       fraction = fractions.Fraction(numerator, denominator)
