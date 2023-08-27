aec5d5537b257d18ff65e68d0a9e137f6fea955d
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 05:20:01 +0000

    Sun, Aug 27, 2023, 12:20 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,8 +3,10 @@ import fractions
 fraction_input = input("Fraction: ").split('/')
 numerator = int(fraction_input[0])
 denominator = int(fraction_input[1])
+print((numerator/denominator)* 100)
 
 try:
       fraction = fractions.Fraction(numerator, denominator)
 except (ValueError, ZeroDivisionError):
     print("Invalid fraction input")
+
