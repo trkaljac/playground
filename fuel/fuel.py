f98340313ef12dea5a824d7d6251089d50b38af7
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 04:53:44 +0000

    Sat, Aug 26, 2023, 11:53 PM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -2,3 +2,10 @@ import fractions
 
 input("Fraction: ")
 
+try:
+    if not input.isnumeric():
+        input = fractions.Fraction(input)
+except ValueError:
+    throw ValueError(myinput + " is not a number")
+except ZeroDivisionError:
+    pass
