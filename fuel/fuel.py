e565f6a9a53056e0ba669c1c1eeb6bc01ddbb727
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 05:36:35 +0000

    Sun, Aug 27, 2023, 12:36 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -8,7 +8,10 @@ denominator = int(fraction_input[1])
 percentage = (numerator / denominator) * 100
 print(f"{percentage:.0f}%")
 
-if pecentage < 
+if pecentage <= "1%":
+    print("E")
+elif pecentage >= "99%":
+    print("E")
 
 try:
       fraction = fractions.Fraction(numerator, denominator)
