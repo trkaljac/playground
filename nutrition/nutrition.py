0ecb6ff9b0f3728fa101acfa67b3c6346a00df25
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 00:01:38 +0000

    Sat, Aug 26, 2023, 7:01 PM -05:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -5,5 +5,5 @@ fruits = {"Apple":130,"Avocado":50,"Sweet Cherries":100,"Banana":110,
           "Tangerine":50,"Watermelon":80}
 
 input("Item: ")
-if input == fruits[1]:
-    print(fruits[2])
+if input == fruits[]:
+    print(fruits[])
