9b88043685e2d301511055e46fd8d218c830c174
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 05:53:33 +0000

    Sun, Aug 27, 2023, 12:53 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,19 +5,18 @@ fraction_input = input("Fraction: ").split('/')
 numerator = int(fraction_input[0])
 denominator = int(fraction_input[1])
 percentage = (numerator / denominator) * 100
-
-if percentage <= 1:
-    print("E")
-elif percentage >= 99:
-    print("F")
-elif print(f"{percentage:.0f}%")
-else:
-
 while True:
+    if percentage <= 1:
+        print("E")
+    elif percentage >= 99:
+        print("F")
+    elif print(f"{percentage:.0f}%"):
+        else:
+            return
     try:
-          fraction = fractions.Fraction(numerator, denominator)
+        fraction = fractions.Fraction(numerator, denominator)
     except (ValueError, ZeroDivisionError):
-          pass
+        pass
 
 
 
