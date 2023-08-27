bf5549d739554df7ccecab6c0270792058bcc582
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 05:16:59 +0000

    Sun, Aug 27, 2023, 12:16 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -5,8 +5,6 @@ numerator = int(fraction_input[0])
 denominator = int(fraction_input[1])
 
 try:
-    fraction = fractions.Fraction(numerator, denominator)
-    percentage = fraction * 100
-    print(f"{fraction.numerator}/{fraction.denominator} is equal to {percentage:.2f}%")
+      fraction = fractions.Fraction(numerator, denominator)
 except (ValueError, ZeroDivisionError):
     print("Invalid fraction input")
