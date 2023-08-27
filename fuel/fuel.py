7550a32f432fcce8e90d1c4cd62b1dc9d7875aac
trkaljac <103012052+trkaljac@users.noreply.github.com>
2023-08-27 05:07:21 +0000

    Sun, Aug 27, 2023, 12:07 AM -05:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,8 +1,8 @@
 import fractions
 
 fractionInput=input("Fraction: ").split('/')
-numerator = int(fraction_input[0])
-denominator = int(fraction_input[1])
+numerator = int(fractionInput[0])
+denominator = int(fractionInput[1])
 
 try:
     if not input.isnumeric():
@@ -10,13 +10,13 @@ try:
 except (ValueError,ZeroDivisionError):
     print(input + " is not a number")
 
-if input == 1/2 :
+if fraction == fractions.Fraction(1, 2):
     print("50%")
-if input == 1/4:
+elif fraction == fractions.Fraction(1, 4):
     print("25%")
-if input == 3/4:
+elif fraction == fractions.Fraction(3, 4):
     print("75%")
-if input == 4/4:
+elif fraction == fractions.Fraction(4, 4):
     print("F")
-if input == 0/4:
+elif fraction == fractions.Fraction(0, 4):
     print("E")
